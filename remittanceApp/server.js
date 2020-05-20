a7a0d338e6c815dd1928c05db686488adcf3d669
Siddharth Shivkumar <siddharth2798@rediffmail.com>
2020-05-20 16:12:30 +0530

    Update server.js

--- remittanceApp/server.js
+++ remittanceApp/server.js
@@ -2,11 +2,7 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
-//var url = "mongodb://localhost:27017";
-
-
-const url = "mongodb+srv://mangrove:yFmFEnBg91NE3GHK@cluster0-maigl.mongodb.net/test?retryWrites=true&w=majority";
-
+var url = "mongodb://localhost:27017";
 
 var cors = require("cors");
 var http = require("http");
